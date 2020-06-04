18834cb2013e598b8fcfab8927cb23aafd865e5a
000Tai <taiu.engineer@gmail.com>
2020-06-04 22:31:18 +0900

    章モジュールを作成します　HeadComp.vue

---
+++ my-form/src/components/modules/HeadComp.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+export default {
+  name: "headComp",
+  data(){
+      return{
+          title: "感想を入力"
+      }
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
