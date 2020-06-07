bbdf65a38c51483d45abe06319dedb76c4216e95
000Tai <taiu.engineer@gmail.com>
2020-06-08 02:31:32 +0900

    【章：thanksページ]

--- my-form/src/router/index.js
+++ my-form/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Form from '@/components/Form';
+import Thanks from '@/components/Thanks';
 
 
 Vue.use(VueRouter);
@@ -15,8 +17,15 @@ const routes = [
     path: "/Form",
     name: "Form",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../components/Form.vue")
+      import(/* webpackChunkName: "form" */ "../components/Form.vue")
+  },
+  {
+    path: "/Thanks",
+    name: "Thanks",
+    component: () =>
+      import(/* webpackChunkName: "thanks" */ "../components/Thanks.vue")
   }
+
 ];
 
 const router = new VueRouter({
